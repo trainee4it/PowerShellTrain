e55fe88f4a47a6c1d823bacd71ff0ea0e564cc58
trainee4it <69849181+trainee4it@users.noreply.github.com>
2023-02-10 13:34:08 +0100

    Add files via upload

---
+++ opzeterrorhandling.ps1
@@ -0,0 +1,26 @@
+﻿$Exists = $True
+
+try
+{
+
+
+    Get-ADOrganizationalUnit -Identity 'ou=support,dc=adatum,dc=com' -ErrorAction Stop
+
+}
+
+catch
+{
+
+
+
+    $exists = $false
+
+}
+
+if($Exists -eq $false)
+{
+
+
+
+
+}
